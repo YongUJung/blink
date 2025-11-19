b448b331f8c1dace7cfeeb078162a561cd6e8330
YongUJung <dyddnwnd4@naver.com>
2025-11-19 11:16:29 +0900

    Modify public domain notice in blink.ino
    Updated public domain notice in the blink example.

--- blink.ino
+++ blink.ino
@@ -2,7 +2,7 @@
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
  
-  This example code is in the public domain.
+  This example code is in the public domain issue1 .
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
